c3ed7755a5504d463b1933ae8b8c741a8ebe8d00
Mayank-Singh-Bhati <121754832+Mayank-Singh-Bhati@users.noreply.github.com>
2023-09-22 01:59:08 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./res/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        pur: "#793EEC",
+        wh: "#F7F6F8",
+        blk: "#444444"
+      }
+    },
+  },
+  plugins: [],
+}
+
